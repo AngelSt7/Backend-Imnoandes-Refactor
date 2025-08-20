3e7d66684167b7126f6eaf60d1a5bdbc16deabb4
AngelSt7 <santacruz232004@gmail.com>
2025-08-20 14:07:42 -0500

    refactor: refactor filter logic to use AND condition for search terms; improve readability and maintainability

--- src/property-me/services/filter/filter.service.ts
+++ src/property-me/services/filter/filter.service.ts
@@ -1,4 +1,5 @@
 
+
 import { Injectable } from '@nestjs/common';
 import { Property } from 'generated/prisma';
 import { PaginationPropertyMeDto } from 'src/property-me/dto';
@@ -6,15 +7,15 @@ import { PaginationPropertyMeDto } from 'src/property-me/dto';
 @Injectable()
 export class FilterService {
     private readonly mode = 'insensitive';
-    private readonly searchFields : (keyof Property)[] = ['name'];
+    private readonly searchFields: (keyof Property)[] = ['name'];
 
     getFilter(query: PaginationPropertyMeDto) {
         const filter: any = {};
 
         if (query.search) {
-            filter.OR = this.searchFields.map(field => ({
-                [field]: { contains: query.search, mode: this.mode },
-            }));
+            const words = query.search.trim().split(/[\s-]+/);
+
+            filter.AND = this.getAndFilter(words);
         }
 
         if (query.minPrice || query.maxPrice) {
@@ -31,8 +32,8 @@ export class FilterService {
             'availability',
         ];
 
-        for( const key of exactFilters) {
-            if(query[key] !== undefined && query[key] !== null) {
+        for (const key of exactFilters) {
+            if (query[key] !== undefined && query[key] !== null) {
                 filter[key] = query[key];
             }
         }
@@ -40,5 +41,24 @@ export class FilterService {
         return filter;
     }
 
+    //Filter by AND
+    //example: filter.AND = this.getAndFilter(words);
+    private getAndFilter(words: string[]) {
+        return words.map(word => ({
+            OR: this.searchFields.map(field => ({
+                [field]: { contains: word, mode: this.mode },
+            })),
+        }));
+    }
+
+    //Filter by OR
+    //example: filter.ORD = this.getOrFilter(words);
+    private getOrFilter(words: string[]) {
+        return words.flatMap(word =>
+            this.searchFields.map(field => ({
+                [field]: { contains: word, mode: this.mode },
+            }))
+        );
+    }
 
 }
