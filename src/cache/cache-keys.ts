a3bbbbdfd76c4b3078e38c8e175dfeee667bce36
AngelSt7 <santacruz232004@gmail.com>
2025-09-07 16:13:29 -0500

    feat: add new cache kesy

--- src/cache/cache-keys.ts
+++ src/cache/cache-keys.ts
@@ -2,6 +2,7 @@ export enum CACHE_KEYS {
     PROPERTIES_ME = 'properties-me',     // listado
     PROPERTY_ME = 'property-me',          // una propiedad
     PROPERTY_DETAIL = 'property-detail',  // detalle de una propiedad
+    PROPERTY_IMAGES = 'property-images',  // imagenes de una propiedad
 
     PROVINCES_TO_DEPARTMENT = 'province-all',    // listado de departamentos
     DISTRICTS_TO_PROVINCE = 'district-all',      // listado de provincias
